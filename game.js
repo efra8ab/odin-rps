3997a8d658f3ad4578491e27ae5beef24f706bf7
efra8ab <efra8ab@gmail.com>
2026-06-12 00:38:19 -0600

    ui for game ready

--- game.js
+++ game.js
@@ -1,4 +1,9 @@
 
+let humanScore = 0
+let computerScore = 0
+const displayUserScore = document.querySelector('.user-score')
+const displayComputerScore = document.querySelector('.computer-score')
+
 const getComputerChoice = () => {
     const options = ['rock', 'paper', 'scissors']
     const randomIndex = Math.floor(Math.random() * options.length)
@@ -6,63 +11,91 @@ const getComputerChoice = () => {
     return randomChoice
 };
 
-const getHumanChoice = () => {
-    const choice = prompt('Enter your choice')
-    return choice
+
+const userSelects = () => {
+
+  const rock = document.querySelector(".select-rock");
+  rock.addEventListener("click", () => {
+    userSelectsHandler(rock)
+  });
+  
+  const paper = document.querySelector(".select-paper");
+  paper.addEventListener("click", () => {
+    userSelectsHandler(paper)
+  });
+  
+  const scissors = document.querySelector(".select-scissors");
+  scissors.addEventListener("click", () => {
+    userSelectsHandler(scissors)
+  });
+
 };
 
-const playGame = () => {
-    let humanScore = 0;
-    let computerScore = 0;
+const userSelectsHandler = (choice) => {
+  const computerChoice = getComputerChoice();
+  playRound(computerChoice, choice.textContent);
+}
+
+const renderScore = () => {
+    displayComputerScore.textContent = computerScore;
+    displayUserScore.textContent = humanScore;
+}
+
 
     const playRound = (ComputerChoice, HumanChoice) => {
+      const result = document.querySelector('.result')
+
+      if (humanScore + computerScore >= 5) {
+        return
+      }
+
       const lowerHumanChoice = HumanChoice.toLowerCase();
 
       if (ComputerChoice === "rock") {
         if (lowerHumanChoice === "rock") {
-          console.log("A tie! .... Try again!");
+          result.textContent = "A tie! .... Try again!";
         } else if (lowerHumanChoice === "paper") {
-          console.log("You won!.... paper beats rock!");
+          result.textContent = "You won!.... paper beats rock!";
           humanScore += 1;
         } else if (lowerHumanChoice === "scissors") {
-          console.log("You lose!... rock beats scissors");
+          result.textContent = "You lose!... rock beats scissors";
           computerScore += 1;
         }
       }
 
       if (ComputerChoice === "paper") {
         if (lowerHumanChoice === "rock") {
-          console.log("You lose!... paper beats rock");
+          result.textContent = "You lose!... paper beats rock";
           computerScore += 1;
         } else if (lowerHumanChoice === "paper") {
-          console.log("A tie! .... Try again!");
+          result.textContent = "A tie! .... Try again!";
         } else if (lowerHumanChoice === "scissors") {
-          console.log("You won!... scissors beats paper");
+          result.textContent = "You won!... scissors beats paper";
           humanScore += 1;
         }
       }
 
       if (ComputerChoice === "scissors") {
         if (lowerHumanChoice === "rock") {
-          console.log("You won!... rock beats scissors");
+          result.textContent = "You won!... rock beats scissors";
           humanScore += 1;
         } else if (lowerHumanChoice === "paper") {
-          console.log("You lose!... scissors beats paper");
+          result.textContent = "You lose!... scissors beats paper";
           computerScore += 1;
         } else if (lowerHumanChoice === "scissors") {
-          console.log("A tie! .... Try again!");
+          result.textContent = "A tie! .... Try again!";
         }
       }
+
+      renderScore()
+
+      if (humanScore + computerScore === 5) {
+        result.textContent = humanScore > computerScore 
+          ? 'You win!!!'
+          : 'Machine wins!!!'
+      }
+
     };
 
-    let n = 1
-    while (n <= 5) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(computerSelection, humanSelection)
-        n += 1
-    }
-
-} 
-
-playGame()
+renderScore()
+userSelects()
